e8444187ab0e785101be9fb02fb57f941e6aa99c
Robert Mafeni <robertmafeni@gmail.com>
2021-04-17 11:58:33 +0100

    updated files, app is working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,20 @@ class PagesController < ApplicationController
   def home
     # work
   end
+
+  def ask
+    # work
+  end
+
+  def answer
+    @user_question = params[:question]
+
+    if @user_question.include? '?'
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    elsif @user_question.downcase == 'i am going to work right now!'
+      @coach_answer = 'Nice one'
+    else
+      @coach_answer = "I don\'t care, get dressed and go to work!"
+    end
+  end
 end
